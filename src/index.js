19a0b9b0a1bebff3783e2887fa578244e5579c91
Luciano dii Souza <contato@thecodelovers.com.br>
2020-03-07 12:56:32 -0300

    At this revision, i am testing componentDidMount, componentWillUnmount and componentDidUpdate component lifecycle with this example, i am able to use this lifecycle inside my React Applications to get data about components when thats events occurs

--- src/index.js
+++ src/index.js
@@ -1,39 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Platzi extends React.Component {
-    state = {
-        texto: 'Oi'
-    }
-    
-    handleButtonClick = () => {
-        this.setState(previousState => {
-            return {
-                texto: 
-                    previousState.texto === 'Oi'
-                        ? 'Tchau'
-                        : 'Oi'
-            };
-        });
-    }; 
-
+class Resultado extends React.Component {
     render() {
         return(
-            <div>
-                <h3 className="platzi"> { this.state.texto} {this.props.nome}</h3>
-                <button onClick={ this.handleButtonClick }>Mudar estado</button>
-            </div>
-
+        <h1> {this.props.resultado} </h1>
         );
+    }
+}
+class Contador extends React.Component {
+    state = {
+        valor: 0
+    }
+    componentDidMount() {
+        this.timerID = setInterval(() => {
+            this.setState(s => ({
+                valor: s.valor + 1
+            }));
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        console.log(prevState.valor, this.state.valor);
+    }
+
+    render() {
+    return <div><Resultado resultado={ this.state.valor } /></div>;
+    }
+}
+
+class App extends React.Component {
+    state = {
+        mostrarContador: true
+    };
+
+    toggleMostrador = () => {
+        this.setState(s => ({
+            mostrarContador: !s.mostrarContador
+        }));
+    };
+
+    render() {
+        if (!this.state.mostrarContador) {
+            return (
+                <div>
+                    <input 
+                    type="checkbox" 
+                    checked={this.state.mostrarContador} 
+                    onChange={this.toggleMostrador} />
+                </div>
+            );
+        }else{
+            return (
+                <div>
+                    <input 
+                    type="checkbox"
+                    checked={ this.state.mostrarContador}
+                    onChange={this.toggleMostrador} />
+                    <Contador />
+                </div>
+            );
+        }
     }
 }
 
 ReactDOM.render(
-    <div>
-        <Platzi nome="Camila" />
-        <Platzi nome="Luciano" />
-    </div>
-,
+    <App />
+    ,
     document.getElementById('root')
 );
