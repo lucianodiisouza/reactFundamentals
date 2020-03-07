c7587b378b54b26ffe50bd40f811bfb5dbaa6f49
Luciano dii Souza <contato@thecodelovers.com.br>
2020-03-07 02:08:19 -0300

    Fist commit for this application, at this checkpoint i have applied dynamic states on class component to change Hello / GoodBye when button component pushed down

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Platzi extends React.Component {
+    state = {
+        texto: 'Oi'
+    }
+    
+    handleButtonClick = () => {
+        this.setState(previousState => {
+            return {
+                texto: 
+                    previousState.texto === 'Oi'
+                        ? 'Tchau'
+                        : 'Oi'
+            };
+        });
+    }; 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return(
+            <div>
+                <h3 className="platzi"> { this.state.texto} {this.props.nome}</h3>
+                <button onClick={ this.handleButtonClick }>Mudar estado</button>
+            </div>
+
+        );
+    }
+}
+
+ReactDOM.render(
+    <div>
+        <Platzi nome="Camila" />
+        <Platzi nome="Luciano" />
+    </div>
+,
+    document.getElementById('root')
+);
